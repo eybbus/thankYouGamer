e1e4d8561da55f5c86899dc088c43bc95b86f45b
eybbus <eythorfreyr94@gmail.com>
2019-11-10 14:00:17 +0000

    Fixed build failure on "new Audio"

--- src/components/AudioContainer.js
+++ src/components/AudioContainer.js
@@ -1,27 +1,40 @@
 import React from "react"
 
-const AudioContainer = props => {
-  const { sound, playOnClick, playOnEnter, endOnLeave } = props
-  let audio = new Audio(sound)
+class AudioContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      audio: null,
+    }
+  }
 
-  if (playOnClick) {
-    return <div onClick={() => audio.play()}>{props.children}</div>
-  } else if (playOnEnter) {
-    return <div onMouseEnter={() => audio.play()}>{props.children}</div>
-  } else if (endOnLeave) {
-    return (
-      <div
-        onMouseEnter={() => audio.play()}
-        onMouseLeave={() => {
-          audio.pause()
-          audio.currentTime = 0
-        }}
-      >
-        {props.children}
-      </div>
-    )
-  } else {
-    return <div onMouseEnter={() => audio.play()}>{props.children}</div>
+  componentDidMount() {
+    this.setState({ audio: new Audio(this.props.sound) })
+  }
+
+  render() {
+    const { playOnClick, playOnEnter, endOnLeave } = this.props
+    const { audio } = this.state
+
+    if (playOnClick) {
+      return <div onClick={() => audio.play()}>{this.props.children}</div>
+    } else if (playOnEnter) {
+      return <div onMouseEnter={() => audio.play()}>{this.props.children}</div>
+    } else if (endOnLeave) {
+      return (
+        <div
+          onMouseEnter={() => audio.play()}
+          onMouseLeave={() => {
+            audio.pause()
+            audio.currentTime = 0
+          }}
+        >
+          {this.props.children}
+        </div>
+      )
+    } else {
+      return <div onMouseEnter={() => audio.play()}>{this.props.children}</div>
+    }
   }
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,26 +4,25 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-import sound from "../assets/throwUp_thankYouGamer.wav"
+import soundThankYou from "../assets/throwUp_thankYouGamer.wav"
+import AudioContainer from "../components/AudioContainer"
 
 const IndexPage = () => {
-  let audio = new Audio(sound)
-  audio.autoplay = true
-
   return (
     <Layout>
       <SEO title="Home" />
-      <div
-        onMouseEnter={() => audio.play()}
-        style={{
-          maxWidth: `75%`,
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          marginBottom: `1.45rem`,
-        }}
-      >
-        <Image />
-      </div>
+      <AudioContainer sound={soundThankYou}>
+        <div
+          style={{
+            maxWidth: `75%`,
+            marginLeft: `auto`,
+            marginRight: `auto`,
+            marginBottom: `1.45rem`,
+          }}
+        >
+          <Image />
+        </div>
+      </AudioContainer>
     </Layout>
   )
 }
